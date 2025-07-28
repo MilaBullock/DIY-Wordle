9c58ed4cc8c0a4dba82027a3b9fed75db6e6c81d
Mila Bullock <milabullock@Milas-MacBook-Pro.local>
2025-07-28 19:05:20 -0400

    finished wordle game

---
+++ Wordle.py
@@ -0,0 +1,78 @@
+def good_word(word:str , length: int):
+    """
+    Checks if the word is a valid English word.
+    """
+    if len(word) != length:
+        print(f"Please enter a word of length {length}.")
+    else:
+        for char in word:
+            if not char.isalpha():
+                return False
+        return True
+        
+
+def new_round():
+    """
+    Starts a new round of the game with the given answer
+    """
+    answer = input("Do you want to play another round? (yes/no)").strip().lower()
+    if answer == "yes":
+        return getting_word()
+    else:
+        print("Thanks for playing!")
+    
+def find_match(guess:str, given_word:str):
+    """
+    Checks guess and given for matches returns emojis based on match
+    
+    in word = yellow
+    right spot = green
+    not in word = black
+    """
+    hint = ""
+    for i in range(len(given_word)):
+        if guess[i] == given_word[i]:
+            hint += "🟩"
+        elif guess[i] in given_word and guess[i] != given_word[i]:
+            hint += "🟨"    
+        else:
+            hint += "⬛"
+    
+    print(hint)
+
+def getting_word():
+    """
+    Gets a random word of a given length from the English words set
+    """
+    import random
+    from english_words import get_english_words_set
+  
+    # getting the random word and length from the user
+    length = int(input("Enter the lenght of the word you want: "))
+    words = get_english_words_set(['web2'], lower=True, alpha=True)
+    given_word = random.choice([word for word in words if len(word) == length])
+    
+    #starting the 5 rounds of the game
+    for i in range(5):
+        guess= input("Enter your guess:")
+        guess = guess.lower()
+
+        # checks if guess is valid and then finds matches
+        if good_word(guess, length) == True:
+            if guess == given_word:
+                print ("Congratulations! You've guessed the word!")
+                return new_round()
+            else:
+                find_match(guess, given_word)
+    
+    print(f"The word was: {given_word}")
+    
+    return new_round()
+
+def main():
+    print(getting_word())
+    
+    
+if __name__ == "__main__":
+    main()
+   
